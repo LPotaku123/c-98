6b72f6feec1ef2fb7e7954bcb90a0ceb91d1fe12
LPotaku123 <142267792+LPotaku123@users.noreply.github.com>
2023-11-13 18:44:25 -0300

    Add files via upload

---
+++ game_login.js
@@ -0,0 +1,15 @@
+// Crie a função addUser()
+function addUser{
+    document.getElementById("player1_name_input").value;
+    document.getElementById("player2_name_input").value;
+    
+    localStorage.setItem("player1_name",player1_name);
+    localStorage.setItem("player2_name",player2_name);
+      
+    }
+   
+  
+  window.location = "game_page.html";
+  
+  
+  
